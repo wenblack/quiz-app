7508d9de83b97a732665ff4cb4dd1353d7e6dcd4
Wender Barbosa <wenblack012@gmail.com>
2024-04-08 15:18:23 -0300

    🦄 refactor: Reset Styles
    Remove default styles from Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,11 +32,6 @@ const config: Config = {
       questions: '12px',
     },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      }
     },
   },
   plugins: [],
