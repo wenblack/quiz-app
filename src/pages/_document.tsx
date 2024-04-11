9e6ce89e65bc33747af7c8f983dfe0f577521c0e
WENDER JOSE BARBOSA SANTOS <wenblack012@gmail.com>
2024-04-11 15:08:53 -0300

    🔧 build: Create PWA
    Create a PWA version of App

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title="👋 QuizTime">
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <meta name="pwa-demo" content="pwa-demo" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="pwa-demo" />
+        <meta name="description" content="pwa-demo" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { apiResponse } from "@/utils/data";
-
 const sora = Sora({ subsets: ["latin"] });
 const MySwal = withReactContent(Swal);
 
@@ -99,31 +98,33 @@ Deseja refazer o teste?`,
   }
 
   return (
-    <main
-      className={`flex flex-col  bg-gray-600 h-screen items-center py-8 px-24 ${sora.className}`}
-    >
-      <Header value={count} />
-      <h1 className="text-h1 h-44 pt-6 lg:h-fit w-screen px-[10vw] text-center text-gray-100 ">
-        {apiResponse[currentPage].question}
-      </h1>
-      <CardView
-        functionShowResult={handleSwitchResult}
-        data={apiResponse[currentPage].answers}
-        showResult={isResult}
-        successFunction={handleUpdateRightAnswers}
-      />
-      <Footer
-        onClick={() => {
-          if (totalPages === currentPage) {
-            Alert(count, totalPages + 1);
-          } else {
-            nextPage();
-          }
-        }}
-        actualQuestion={currentPage + 1}
-        totalQuestion={totalPages + 1}
-        isDisabled={isResult}
-      />
-    </main>
+    <>
+      <main
+        className={`flex flex-col  bg-gray-600 h-screen items-center py-8 px-24 ${sora.className}`}
+      >
+        <Header value={count} />
+        <h1 className="text-h1 h-44 pt-6 lg:h-fit w-screen px-[10vw] text-center text-gray-100 ">
+          {apiResponse[currentPage].question}
+        </h1>
+        <CardView
+          functionShowResult={handleSwitchResult}
+          data={apiResponse[currentPage].answers}
+          showResult={isResult}
+          successFunction={handleUpdateRightAnswers}
+        />
+        <Footer
+          onClick={() => {
+            if (totalPages === currentPage) {
+              Alert(count, totalPages + 1);
+            } else {
+              nextPage();
+            }
+          }}
+          actualQuestion={currentPage + 1}
+          totalQuestion={totalPages + 1}
+          isDisabled={isResult}
+        />
+      </main>
+    </>
   );
 }
